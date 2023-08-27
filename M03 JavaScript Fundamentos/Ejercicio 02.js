aff1b29a1613a34afe5b107e44d22899391bd4c3
seonauta2 <bajista7@gmail.com>
2023-08-27 19:49:31 -0400

    resolucion de ejercicios de M03 JavaScript

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,8 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
+   
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +13,37 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   const resultado = x + y;
+   return resultado;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   const sustraccion = x - y;
+   return sustraccion;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   const division = x / y;
+   return division;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   const multiplicacion = x * y;
+   return multiplicacion;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   const resto = x % y;
+   return resto;
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
